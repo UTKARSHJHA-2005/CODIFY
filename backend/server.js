ab80117462e46ba0178674da47d4ffc374049619
UTKARSH JHA <147072811+UTKARSHJHA-2005@users.noreply.github.com>
2025-07-29 12:58:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -76,7 +76,9 @@ io.on("connection", (socket) => {
         delete usersocketmap[socket.id];
     });
 });
-
+app.get("/", (req, res) => {
+  res.send("Socket backend is live!");
+});
 // Starts the server on port 3000.
 const PORT = 3000;
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
